9ac2061020852814daa73bd63c2950311d196d5e
jchck <hey@justinchick.com>
2016-04-27 16:55:06 -0400

    enque stylesheet

--- lib/assets.php
+++ lib/assets.php
@@ -1,3 +1,9 @@
 <?php
 
 namespace jchck\assets;
+
+function assets(){
+	wp_enqueue_style('css', get_template_directory_uri() . '/dist/styles.css', false, null);
+}
+
+add_action('wp_enqueue_scripts', __NAMESPACE__ . '\\assets', 100);
