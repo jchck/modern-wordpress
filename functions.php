c714b7f2daed791fffe1143905a708773136ed75
jchck <hey@justinchick.com>
2016-04-27 15:45:35 -0400

    setup the functions directory

---
+++ functions.php
@@ -0,0 +1,16 @@
+<?php
+
+$jchck_includes = [
+	'lib/assets.php',
+	'lib/cleanup.php'
+];
+
+foreach ($jchck_includes as $file) {
+	if (!$filepath = locate_template( $file )) {
+		trigger_error(sprintf(__('Error locating %s for inclusion', 'jchck'), $file), E_USER_ERROR);
+	}
+
+	require_once $filepath;
+}
+
+unset($file, $filepath);
