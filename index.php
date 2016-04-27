e1d994a3fe1f7e9cd9ecf01da4ea9b45d6ef0a89
jchck <hey@justinchick.com>
2016-04-27 15:45:17 -0400

    basic wordpress ready template

--- index.php
+++ index.php
@@ -2,11 +2,12 @@
 <html>
 <head>
 	<meta charset="utf-8">
-	<title></title>
-	<meta name="description" content="">
-	<meta name="author" content="">
+	<title><?php bloginfo( 'name' ); ?></title>
+	<meta name="description" content="<?php echo bloginfo( 'description' ); ?>">
+	<meta name="author" content="<?php echo bloginfo( 'name' ); ?>">
+	<meta http-equiv="x-ua-compatible" content="ie=edge">
 	<meta name="viewport" content="width=device-width,initial-scale=1">
-	<link rel="stylesheet" href="">
+	<?php wp_head(); ?>
 </head>
 <body>
 
