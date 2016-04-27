1b814cccfd53da419eb321378d53e9fb8c0b627d
jchck <hey@justinchick.com>
2016-04-27 16:18:34 -0400

    use soil plugin to clean the <head> block

---
+++ lib/cleanup.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace jchck\cleanup;
+
+function cleanup(){
+	add_theme_support('soil-clean-up');
+	add_theme_support('soil-nav-walker');
+	add_theme_support('soil-nice-search');
+	add_theme_support('soil-jquery-cdn');
+	add_theme_support('soil-relative-urls');
+}
+
+add_action('after_setup_theme', __NAMESPACE__ . '\\cleanup');
